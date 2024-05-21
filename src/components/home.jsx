4bf0b92a981ce9bb7f33009328ff9d6c3311dede
Paige Nakai <86543892+pmnak@users.noreply.github.com>
2024-05-21 12:47:55 -0400

    added in book search

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Checkbox } from '@mantine/core';
+import {
+  Autocomplete, Select, Text, Group, Title, Container,
+} from '@mantine/core';
 import useStore from '../store';
 
 function Home() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Container style={{ width: '75vw', margin: '0 auto', paddingTop: '5vh' }}>
+      <Title order={2} align="center" style={{ marginBottom: '20px' }}>
+        Explore a New Book Trade
+      </Title>
+      <Group style={{ display: 'flex', gap: '10px' }}>
+        <Select
+          label="Search By"
+          placeholder="Filter"
+          data={['Genre', 'Author', 'Condition', 'Length']}
+          style={{ flex: 1 }}
+        />
+        <Autocomplete
+          label="Find Book"
+          placeholder="Enter Title or Author"
+          style={{ flex: 3 }}
+        />
+      </Group>
+    </Container>
   );
 }
 

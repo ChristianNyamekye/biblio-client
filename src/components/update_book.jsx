7964a6774f9b55a1d042aa05ad7199254f1e491b
jeanmarcosp <jeanmarcos.perez.24@dartmouth.edu>
2024-05-21 11:41:33 -0400

    linting errors

--- src/components/update_book.jsx
+++ src/components/update_book.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 function UpdateBook() {
   return (
-<<<<<<< HEAD
     <div>This is for handling upload book</div>
-=======
-    <div>Displaybook</div>
->>>>>>> eed9b1c
   );
 }
 

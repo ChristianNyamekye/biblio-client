ca63a30bb61512f5662ddf2b4fe405bed613fab7
abdibaset <abdibaset.a.bare.25@dartmouth.edu>
2024-05-22 20:08:11 -0400

    clean up

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import React, { useState, useEffect, useCallback } from 'react';
-=======
-import React, { useState, useEffect } from 'react';
->>>>>>> 900a7c6
 import {
   Autocomplete, Select, Title, Container, Loader, Button,
 } from '@mantine/core';
 import axios from 'axios';
-<<<<<<< HEAD
 import debounce from 'lodash.debounce';
 
 function Home() {
@@ -15,13 +10,6 @@ function Home() {
   const [lastSearchTerm, setLastSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
-=======
-import useStore from '../store';
-
-function Home() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
->>>>>>> 900a7c6
 
   const filterUniqueBooks = (books) => {
     const seenTitles = new Set();
@@ -34,7 +22,6 @@ function Home() {
     });
   };
 
-<<<<<<< HEAD
   const fetchBooks = useCallback(
     debounce(async (query) => {
       if (!query) return;
@@ -69,34 +56,11 @@ function Home() {
     }
   };
 
-=======
-  useEffect(() => {
-    if (searchTerm) {
-      console.log('Fetching books for:', searchTerm);
-      const fetchBooks = async () => {
-        try {
-          const response = await axios.get('https://project-api-biblio.onrender.com/api/books/search', {
-            params: { query: searchTerm },
-          });
-          const uniqueBooks = filterUniqueBooks(response.data.items || []);
-          setSearchResults(uniqueBooks); // Ensure searchResults is always an array
-        } catch (error) {
-          setSearchResults([]); // Set to empty array on error
-        }
-      };
-      fetchBooks();
-    } else {
-      setSearchResults([]); // Reset when search term is empty
-    }
-  }, [searchTerm]);
-
->>>>>>> 900a7c6
   return (
     <Container style={{ width: '75vw', margin: '0 auto', paddingTop: '5vh' }}>
       <Title order={2} align="center" style={{ marginBottom: '20px' }}>
         Explore a New Book Trade
       </Title>
-<<<<<<< HEAD
       <Select
         label="Search By"
         placeholder="Filter"
@@ -121,27 +85,6 @@ function Home() {
       >
         Submit
       </Button>
-=======
-      <Group style={{ display: 'flex', gap: '10px' }}>
-        <Select
-          label="Search By"
-          placeholder="Filter"
-          data={['Genre', 'Author', 'Condition', 'Length']}
-          style={{ flex: 1 }}
-        />
-        <Autocomplete
-          label="Find Book"
-          placeholder="Enter Title or Author"
-          data={searchResults.map((book) => ({
-            value: book.volumeInfo.title,
-            label: book.volumeInfo.title,
-            description: book.volumeInfo.authors?.join(', '),
-          }))}
-          style={{ flex: 3 }}
-          onChange={(value) => setSearchTerm(value)}
-        />
-      </Group>
->>>>>>> 900a7c6
     </Container>
   );
 }

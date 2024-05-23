19760e8daed226d64d6770799442c0966eda5e22
Paige Nakai <86543892+pmnak@users.noreply.github.com>
2024-05-23 01:48:17 -0400

    added in footer

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import {
-  BrowserRouter, Routes, Route,
-} from 'react-router-dom';
-import { BackgroundImage } from '@mantine/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './nav-bar';
 import Home from './home';
 import Profile from './profile/profile';
 import Login from './login';
 import Signup from './signup';
 import BookModal from './bookModal';
-import responseModal from './responseModal';
+import ResponseModal from './responseModal';
 import UploadBook from './upload_book';
 import DisplayBook from './display_book';
 import UpdateBook from './update_book';
 import Education from './education';
 import Onboarding from './onboarding';
+import Footer from './footer';
 import '@mantine/core/styles.css';
 
 function FallBack(props) {
@@ -24,19 +22,22 @@ function FallBack(props) {
 function App(props) {
   return (
     <BrowserRouter>
-      <div>
+      <div className="App">
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/bookModal" element={<BookModal />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-          <Route path="/how-it-works" element={<Education />} />
-          <Route path="/onboarding" element={<Onboarding />} />
-          <Route path="*" element={<FallBack />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Signup />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/bookModal" element={<BookModal />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+            <Route path="/how-it-works" element={<Education />} />
+            <Route path="/onboarding" element={<Onboarding />} />
+            <Route path="*" element={<FallBack />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </BrowserRouter>
   );

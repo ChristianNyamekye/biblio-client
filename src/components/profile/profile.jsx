26837c8686007d8db4a4a35145017ef9624ab565
jeanmarcosp <jeanmarcos.perez.24@dartmouth.edu>
2024-05-25 17:17:30 -0400

    Revert "Merge pull request #75 from dartmouth-cs52-24s/cn_biblio_client"
    This reverts commit f3189f930f5de7b56a69cb8b0335402bb960650a, reversing
    changes made to e76a91e4a081bd18dacdaa9468b9f209963b0cc0.

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -40,16 +40,11 @@ function Profile() {
 
   const renderContent = () => {
     switch (activeTab) {
-      case 'library':
-        return <Library userId={userId} />;
-      case 'offers':
-        return <ActiveOffers />;
-      case 'wishlist':
-        return <Wishlist userId={userId} />;
-      case 'settings':
-        return <Settings />;
-      default:
-        return null;
+      case 'library': return <Library userId={userId} />;
+      case 'offers': return (<ActiveOffers />);
+      case 'wishlist': return (<Wishlist />);
+      case 'settings': return (<Settings />);
+      default: return null;
     }
   };
 

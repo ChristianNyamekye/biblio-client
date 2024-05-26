6ba002acc00ad054e2d3be992036b9fc988a2845
Maya Cole <maya.cole@dali.dartmouth.edu>
2024-05-26 12:10:33 -0400

    updatedTradeModalFlow

--- src/components/tradeModal.jsx
+++ src/components/tradeModal.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  Modal, Button, Text, Card, Image,
+  Modal, Button, Text, Card, Image, Select,
 } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
+import useStore from '../store';
 
 function TradeModal({
-  isOpen, onClose, username, book,
+  isOpen, onClose, user, book,
 }) {
   const navigate = useNavigate();
+  const [selectedBookId, setSelectedBookId] = useState('');
+  const [tradeStatus, setTradeStatus] = useState('');
+  const { currUserBooks, fetchUserBooks } = useStore((state) => ({
+    currUserBooks: state.biblioSlice.currUserBooks,
+    fetchUserBooks: state.biblioSlice.fetchUserBooks,
+  }));
+
+  useEffect(() => {
+    fetchUserBooks();
+  }, []);
+
+  const handleTradeRequest = () => {
+    setTradeStatus('sent');
+  };
 
   const handleKeepLooking = () => {
     navigate('/home');
     onClose();
   };
 
-  if (!book) {
-    return null;
-  }
+  if (!book) return null;
 
   return (
     <Modal
       opened={isOpen}
       onClose={onClose}
-      title="Trade Request Sent"
+      title={tradeStatus ? 'Trade Request Sent' : 'Initiate Trade Request'}
       size="md"
     >
       <Card shadow="sm" padding="lg">
@@ -33,12 +46,35 @@ function TradeModal({
             style={{ maxWidth: '50%', height: 'auto' }}
           />
         </Card.Section>
-        <Text
-          size="md"
-          style={{ marginTop: 14, textAlign: 'center' }}
-        >
-          Your trade offer has been sent! Check back later to see if they accept.
-        </Text>
+        {tradeStatus ? (
+          <Text size="md" style={{ marginTop: 14, textAlign: 'center' }}>
+            Congrats! Your trade offer has been sent! Check back later to see if they accept.
+          </Text>
+        ) : (
+          <>
+            <Text
+              size="md"
+              style={{ marginTop: 14, textAlign: 'center' }}
+            >
+              Select a book from your library to trade for {book.title}.
+            </Text>
+            <Select
+              label="Choose your book to trade:"
+              placeholder="Select a book"
+              data={currUserBooks.map(({ id, title }) => ({ value: id, label: title }))}
+              value={selectedBookId}
+              onChange={setSelectedBookId}
+              style={{ marginTop: 14 }}
+            />
+            <Button
+              fullWidth
+              style={{ marginTop: 14 }}
+              onClick={handleTradeRequest}
+            >
+              Send Trade Request
+            </Button>
+          </>
+        )}
         <Button
           fullWidth
           style={{ marginTop: 14 }}

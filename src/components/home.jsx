1682794011f4e19e24fc764086f023e076b38c25
jeanmarcosp <jeanmarcos.perez.24@dartmouth.edu>
2024-05-26 12:44:38 -0400

    home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,16 +8,31 @@ import {
   Button,
   Group,
   Text,
+  Card,
+  Image,
+  SimpleGrid,
 } from '@mantine/core';
-import axios from 'axios';
+import axios, { all } from 'axios';
 import debounce from 'lodash.debounce';
-import useStore from '../store/biblio-store_slice';
+import { IconCirclePlus, IconHeart, IconTrash } from '@tabler/icons-react';
+import useStore from '../store';
+import BookModal from './bookModal';
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState('');
   const [lastSearchTerm, setLastSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  const fetchAllBooks = useStore(({ biblioSlice }) => biblioSlice.fetchAllBooks);
+  const allBooks = useStore(({ biblioSlice }) => biblioSlice.allBooks);
+
+  useEffect(() => {
+    fetchAllBooks();
+  }, []);
+
+  console.log(allBooks);
 
   const filterUniqueBooks = (books) => {
     const seenTitles = new Set();
@@ -70,43 +85,95 @@ function Home() {
     }
   };
 
+  const [bookDetailsOpened, setBookDetailsOpened] = useState(false);
+
+  const handleViewBook = (book) => {
+    setSelectedBook(book);
+    setBookDetailsOpened(true);
+  };
+
   return (
-    <Container style={{ width: '75vw', margin: '0 auto', paddingTop: '5vh' }}>
-      <Title order={2} align="center" style={{ marginBottom: '20px' }}>
-        Explore a New Book Trade
-      </Title>
-      <Group style={{ display: 'flex', gap: '10px' }}>
-        <Select
-          label="Search By"
-          placeholder="Filter"
-          data={['Genre', 'Author', 'Condition', 'Length']}
-          style={{ flex: 1 }}
-        />
-        <Autocomplete
-          label="Find Book"
-          placeholder="Enter Title or Author"
-          data={searchResults.map((book) => ({
-            value: book.volumeInfo.title,
-            label: book.volumeInfo.title,
-            description: book.volumeInfo.authors?.join(', '),
-          }))}
-          onChange={(value) => {
-            setSearchTerm(value);
-            setLastSearchTerm(value);
-          }}
-          rightSection={loading ? <Loader size="sm" /> : null}
-          style={{ flex: 3 }}
-        />
-        <Button
-          onClick={handleSubmit}
-          color="indigo"
-          disabled={loading}
-          style={{ flex: 1, marginTop: '20px' }}
-        >
-          Submit
-        </Button>
-      </Group>
-    </Container>
+    <div>
+      <Container style={{ width: '75vw', margin: '0 auto', paddingTop: '5vh' }}>
+        <Title order={2} align="center" style={{ marginBottom: '20px' }}>
+          Explore a New Book Trade
+        </Title>
+        <Group style={{ display: 'flex', gap: '10px' }}>
+          <Select
+            label="Search By"
+            placeholder="Filter"
+            data={['Genre', 'Author', 'Condition', 'Length']}
+            style={{ flex: 1 }}
+          />
+          <Autocomplete
+            label="Find Book"
+            placeholder="Enter Title or Author"
+            data={searchResults.map((book) => ({
+              value: book.volumeInfo.title,
+              label: book.volumeInfo.title,
+              description: book.volumeInfo.authors?.join(', '),
+            }))}
+            onChange={(value) => {
+              setSearchTerm(value);
+              setLastSearchTerm(value);
+            }}
+            rightSection={loading ? <Loader size="sm" /> : null}
+            style={{ flex: 3 }}
+          />
+          <Button
+            onClick={handleSubmit}
+            color="indigo"
+            disabled={loading}
+            style={{ flex: 1, marginTop: '20px' }}
+          >
+            Submit
+          </Button>
+        </Group>
+      </Container>
+
+      <div className="library-card-holder">
+        {allBooks.map((book) => (
+          <Card
+            key={book.id}
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            className="post-card"
+          >
+            <Card.Section>
+              <Image
+                src={book.coverImage}
+                height={300}
+                width={0}
+                alt={book.title}
+              />
+            </Card.Section>
+            <Group position="apart" mt="md" mb="xs">
+              <Text fw={500}>{book.title}</Text>
+            </Group>
+            <Text size="sm" c="dimmed">
+              {book.author}
+            </Text>
+            <SimpleGrid cols={1}>
+              <Button
+                color="indigo"
+                fullWidth
+                mt="md"
+                radius="md"
+                onClick={() => handleViewBook(book)}
+              >
+                View Book
+              </Button>
+            </SimpleGrid>
+          </Card>
+        ))}
+      </div>
+      <BookModal
+        opened={bookDetailsOpened}
+        onClose={() => setBookDetailsOpened(false)}
+        book={selectedBook}
+      />
+    </div>
   );
 }
 
